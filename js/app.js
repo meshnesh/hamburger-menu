faeb30a1f6124810b74c2c97ecf70ea21a8864ea
meshnesh <tonnie.nesh@gmail.com>
2016-05-17 21:43:01 +0300

    solved the slide issue

--- js/app.js
+++ js/app.js
@@ -1,32 +1,21 @@
-// HAMBURGLERv2
-
-// function togglescroll() {
-//   $('body').on('touchstart', function(e) {
-//     if ($('body').hasClass('noscroll')) {
-//       e.preventDefault();
-//     }
-//   });
-// }
-
-// $(document).ready(function() {
-//   togglescroll()
-//   $(".icon").click(function() {
-//     $(".mobilenav").fadeToggle(500);
-//     $(".top-menu").toggleClass("top-animate");
-//     $("body").toggleClass("noscroll");
-//     $(".mid-menu").toggleClass("mid-animate");
-//     $(".bottom-menu").toggleClass("bottom-animate");
-//   });
-// });
-
-// // PUSH ESC KEY TO EXIT
-
-// $(document).keydown(function(e) {
-//   if (e.keyCode == 27) {
-//     $(".mobilenav").fadeOut(500);
-//     $(".top-menu").removeClass("top-animate");
-//     $("body").removeClass("noscroll");
-//     $(".mid-menu").removeClass("mid-animate");
-//     $(".bottom-menu").removeClass("bottom-animate");
-//   }
-// });
+// jQuery(document).ready(function($){
+//      $(".menu-item-has-children").append("<div class='open-menu-link open'>+</div>");
+//      $('.menu-item-has-children').append("<div class='open-menu-link close'>-</div>");
+ 
+//      $('.open').addClass('visible');
+ 
+//      $('.open-menu-link').click(function(e){
+//          var childMenu =  e.currentTarget.parentNode.children[1];
+//          if($(childMenu).hasClass('visible')){
+//              $(childMenu).removeClass("visible");
+ 
+//              $(e.currentTarget.parentNode.children[3]).removeClass("visible");
+//              $(e.currentTarget.parentNode.children[2]).addClass("visible");
+//          } else {
+//              $(childMenu).addClass("visible");
+ 
+//              $(e.currentTarget.parentNode.children[2]).removeClass("visible");
+//              $(e.currentTarget.parentNode.children[3]).addClass("visible");
+//          }
+//      });
+//  });
